c452fa43005bd3a22781ef862787812267c39687
AyanQuadri <zenithayan@hotmail.com>
2025-10-28 17:38:38 +0530

    testing math module

--- package/src/main.rs
+++ package/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     let reversed: String = text_library::reverse("Rust");
     println!("Reversed value: {}", reversed);
+
+    let result = text_library::maths::add(5, 19);
+    println!("Result = {}", result);
+
+    let result = text_library::maths::sub(8, 5);
+    println!("Result = {}", result);
 }
