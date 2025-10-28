61dda3fce4d02dd68c431537930aed2509c41a51
AyanQuadri <zenithayan@hotmail.com>
2025-10-28 17:38:06 +0530

    add math module

--- package/text_library/src/lib.rs
+++ package/text_library/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod maths;
+
 pub fn reverse(input: &str) -> String {
     input.chars().rev().collect()
 }
@@ -9,6 +11,16 @@ pub fn is_palindrome(input: &str) -> bool {
         .collect::<String>()
         .to_lowercase();
     cleaned_input == reverse(&cleaned_input)
+}
+
+pub fn demo_add() {
+    let result = maths::add(5, 8);
+    println!("5 + 8 = {}", result);
+}
+
+pub fn demo_sub() {
+    let result = maths::sub(9, 2);
+    println!("Result = {}", result);
 }
 
 #[cfg(test)]
@@ -23,5 +35,15 @@ mod tests {
     #[test]
     fn text_is_palindrome() {
         assert!(is_palindrome("A man, a plan, a canal, Panama"));
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(maths::add(2, 3), 5);
+    }
+
+    #[test]
+    fn test_sub() {
+        assert_eq!(maths::sub(8, 3), 5);
     }
 }
