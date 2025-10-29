cb806f1c7bba1a6c39c5ff1bf879c7a40adda682
AyanQuadri <zenithayan@hotmail.com>
2025-10-29 19:00:54 +0530

    add parsing example with struct

--- cli-tool/src/main.rs
+++ cli-tool/src/main.rs
@@ -1,3 +1,27 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "greeter")]
+#[command(about = "A simple Cli tool to greet a person", long_about = "None")]
+struct Greet {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = String::from("Hello"))]
+    greeting: String,
+
+    #[arg(short, long, action = clap::ArgAction::SetTrue)]
+    uppercase: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Greet = Greet::parse();
+
+    let mut message: String = format!("{}, {}!", args.greeting, args.name);
+
+    if args.uppercase {
+        message = message.to_uppercase();
+    }
+
+    println!("{}", message);
 }
